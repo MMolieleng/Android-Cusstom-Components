071c9b6a9919aa44c2ac3349071c1f45a0a54a6b
Mohale Molieleng <mjmolieleng@gmail.com>
2020-07-08 01:31:26 +0200

    Revert "added some components"
    This reverts commit 9e48e24c86a0d56d965a2563e7255ada259e76dc.
    remove some commit

--- app/src/main/java/com/techforall/usingcustomcomponents/model/Product.kt
+++
@@ -1,4 +0,0 @@
-package com.techforall.usingcustomcomponents.model
-
-data class Product(val title: String, val price: Double, val category: String, val imageUrl: String) {
-}
--- app/src/main/java/com/techforall/usingcustomcomponents/model/Skill.kt
+++
@@ -1,4 +0,0 @@
-package com.techforall.usingcustomcomponents.model
-
-data class Skill(val skillTitle: String, val skillLevel: String) {
-}
