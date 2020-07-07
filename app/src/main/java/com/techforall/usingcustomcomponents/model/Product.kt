9e48e24c86a0d56d965a2563e7255ada259e76dc
Mohale Molieleng <mmolieleng@jhb.dvt.co.za>
2020-07-08 01:22:57 +0200

    added some components

---
+++ app/src/main/java/com/techforall/usingcustomcomponents/model/Product.kt
@@ -0,0 +1,4 @@
+package com.techforall.usingcustomcomponents.model
+
+data class Product(val title: String, val price: Double, val category: String, val imageUrl: String) {
+}
---
+++ app/src/main/java/com/techforall/usingcustomcomponents/model/Skill.kt
@@ -0,0 +1,4 @@
+package com.techforall.usingcustomcomponents.model
+
+data class Skill(val skillTitle: String, val skillLevel: String) {
+}
